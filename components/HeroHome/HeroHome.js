faaa5de5f1f8c919d402e973a37b228e49ab062b
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-05-30 19:32:17 +0700

    [Add+Refactor] peta-indonesia image & refactor theme provider on Homepage to HeroHome

--- components/HeroHome/HeroHome.js
+++ components/HeroHome/HeroHome.js
@@ -1,69 +1,77 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { Typography } from '@material-ui/core';
+import { Typography, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import NextIcon from '@material-ui/icons/NavigateNext';
 
 import Button from '../Button/Button';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+});
+
 export default function HeroHome() {
   return (
-    <div className="hero-home-container">
-      <div className="content-wrapper">
-        <div className="top-wrapper">
-          <div className="hero-logo-wrapper">
-            <img src="/images/logo-home.png" alt="logo loakarya" />
+    <ThemeProvider theme={theme}>
+      <div className="hero-home-container">
+        <div className="content-wrapper">
+          <div className="top-wrapper">
+            <div className="hero-logo-wrapper">
+              <img src="/images/logo-home.png" alt="logo loakarya" />
+            </div>
+            <ul className="menu-wrapper">
+              <li className="hero-menu-item">
+                <a href="#">Masuk</a>
+              </li>
+              <li className="hero-menu-item">
+                <a href="#">Daftar</a>
+              </li>
+            </ul>
           </div>
-          <ul className="menu-wrapper">
-            <li className="hero-menu-item">
-              <a href="#">Masuk</a>
-            </li>
-            <li className="hero-menu-item">
-              <a href="#">Daftar</a>
-            </li>
-          </ul>
-        </div>
 
-        <div className="hero-content">
-          <Typography align="left" variant="h1" className="hero-title">
-            Zero Waste.
-          </Typography>
-          <Typography align="left" variant="h1" className="hero-title">
-            Green Material.
-          </Typography>
-          <Typography align="left" variant="h1" className="hero-title">
-            Sustainable creation for you.
-          </Typography>
+          <div className="hero-content">
+            <Typography align="left" variant="h1" className="hero-title">
+              Zero Waste.
+            </Typography>
+            <Typography align="left" variant="h1" className="hero-title">
+              Green Material.
+            </Typography>
+            <Typography align="left" variant="h1" className="hero-title">
+              Sustainable creation for you.
+            </Typography>
 
-          <Typography
-            align="left"
-            variant="subtitle1"
-            className="hero-subtitle"
-          >
-            Kurangi limbah. Kreasikan rumah. Furniture dan dekorasi yang membuat
-            indah kediaman anda. Menggunakan material ramah lingkungan dan
-            dengan proses yang menyenangkan.
-          </Typography>
+            <Typography
+              align="left"
+              variant="subtitle1"
+              className="hero-subtitle"
+            >
+              Kurangi limbah. Kreasikan rumah. Furniture dan dekorasi yang
+              membuat indah kediaman anda. Menggunakan material ramah lingkungan
+              dan dengan proses yang menyenangkan.
+            </Typography>
 
-          <p>Pesan dan bantu selamatkan lingkungan sekarang!</p>
-          <div className="hero-button-wrapper">
-            <Button
-              text="Beli produk"
-              classNames="btn-can-hover-green"
-              endIcon={<NextIcon />}
-              link="https://www.google.com"
-            />
-            <Button
-              text="Kreasikan ruanganmu"
-              classNames="btn-can-hover-green"
-              endIcon={<NextIcon />}
-            />
+            <p>Pesan dan bantu selamatkan lingkungan sekarang!</p>
+            <div className="hero-button-wrapper">
+              <Button
+                text="Beli produk"
+                classNames="btn-can-hover-green"
+                endIcon={<NextIcon />}
+                link="https://www.google.com"
+              />
+              <Button
+                text="Kreasikan ruanganmu"
+                classNames="btn-can-hover-green"
+                endIcon={<NextIcon />}
+              />
+            </div>
+            <a href="#" className="link-hubungi-kami">
+              Hubungi Kami
+            </a>
           </div>
-          <a href="#" className="link-hubungi-kami">
-            Hubungi Kami
-          </a>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
